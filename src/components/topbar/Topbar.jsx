9e97beca61c71fc5829480c65cdba7dddef33e7e
ZachRochette <zachrochette@hotmail.com>
2022-05-28 04:43:15 -0600

    downloaded react and material icons started design

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,42 @@
+import "./topbar.css";
+import PersonIcon from '@mui/icons-material/Person';
+import SearchIcon from '@mui/icons-material/Search';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+export default function Topbar() {
+    return (
+        <div className="topbarContainer">
+            <div className="topbarLeft">
+                <span className="logo">Social Media</span>
+            </div>
+            <div className="topbarCenter">
+                <div className="searchbar">
+                    <SearchIcon className="search" />
+                    <input placeholder="Search for friends, posts or video" className="searchInput" />
+                </div>
+            </div>
+            <div className="topbarRight">
+                <div className="topbarLinks">
+                    <span className="topbarLink">Homepage</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcon">
+                    <div className="topbarIconItem">
+                        <PersonIcon />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <ChatIcon />
+                        <span className="topbarIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <NotificationsIcon />
+                        <span className="topbarIconBadge">10</span>
+                    </div>
+                </div>
+                <img src="/assets/person/8.jpg" alt="" className="topbarImg" />
+            </div>
+        </div>
+    );
+}
