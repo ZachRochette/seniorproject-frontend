389c2c7f20a8155d6cf3ab1546382df6c095f476
ZachRochette <zachrochette@hotmail.com>
2022-05-30 04:08:53 -0600

    Profile Page

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,32 @@
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+
+export default function Profile() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img className="profileCoverImg" src="assets/post/5.png" alt="" />
+              <img className="profileUserImg" src="assets/person/8.jpg" alt="" />
+            </div>
+            <div className="profileInfo">
+                <h4 className="profileInfoName">Zach Rochette</h4>
+                <span className="profileInfoDesc">Hello my friends!</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile/>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
